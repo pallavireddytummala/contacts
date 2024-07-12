c46a50ee91e1795b4297b49d5e2e38c810ac971d
Tummala Pallavi Reddy <pallavireddytummala2003@gmail.com>
2024-07-12 19:15:31 +0530

    Add files via upload

---
+++ header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <div className="header-info">
+          Consulting WP<span className="header-bullet"></span>
+          New York<span className="header-bullet"></span>
+          Services
+        </div>
+        <nav className="header-nav">
+          <a href="/services" className="header-link">
+            Services
+            <div className="header-underline"></div>
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
